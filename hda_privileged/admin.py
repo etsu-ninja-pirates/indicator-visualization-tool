f937d5f483c5b0b2c1a80c30a5a581a7e26839a6
Johnny E <nshimiyejm@gmail.com>
2018-09-02 20:14:57 -0400

    privileged user model update
    Add models that will serve as tables in the DB to store the data that will be uploaded by the privileged user. This data will be contained on a CSV file.

--- hda_privileged/admin.py
+++ hda_privileged/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .import models
 
 # Register your models here.
+admin.site.register(models.US_States)
+admin.site.register(models.US_Counties)
+admin.site.register(models.Data_Point)
+admin.site.register(models.Data_Set)
+admin.site.register(models.Indicators)
--- hda_privileged/models.py
+++ hda_privileged/models.py
@@ -1,3 +1,37 @@
+import datetime
 from django.db import models
 
 # Create your models here.
+class Indicators(models. Model):
+    indicator_id = models.BigIntegerField(primary_key=True)
+    name = models.CharField(max_length=200)
+    
+class Data_Set(models.Model):
+    data_id = models.BigIntegerField(primary_key=True)
+    year = models.DateField()
+    auther = models.CharField(max_length=200)
+    
+    indicator_id = models.ForeignKey(Indicators, on_delete=models.CASCADE)
+
+class US_States(models.Model): 
+    state_id = models.BigIntegerField(primary_key=True)
+    short_name = models.CharField(max_length=200)
+    state_name = models.CharField(max_length=200)
+    s_fips = models.BigIntegerField(default = 0)
+
+class US_Counties(models.Model):
+    county_id = models.BigIntegerField(primary_key=True)
+    county_name = models.CharField(max_length=200)
+    c_fips = models.IntegerField()
+
+    state_id = models.ForeignKey(US_States, on_delete=models.CASCADE)
+
+class Data_Point(models.Model):
+    point_id = models.BigIntegerField(primary_key=True)
+    value = models.IntegerField(default= 0)
+    precentile = models.FloatField(default= 0)
+    source = models.CharField(max_length=200)
+    interval = models.CharField(max_length=200)
+
+    data_id = models.ForeignKey(Data_Set, on_delete=models.CASCADE)
+    county_id = models.ForeignKey(US_States, on_delete=models.CASCADE)
