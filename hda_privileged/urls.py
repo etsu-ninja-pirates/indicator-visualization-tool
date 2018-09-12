a3a8517ee9607c85909a754e95b5ac6e5ac652c1
Kim Hawkins <hawkinska1@goldmail.etsu.edu>
2018-09-12 18:52:58 -0400

    DES-39 Priv Navigation
    Testing navigation for privileged user pages

--- hda_privileged/urls.py
+++ hda_privileged/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('sample/', views.sampleNavBar, name='sample'),
     path('login/', views.user_login, name='login'),
     path('metric/', views.manage_metrics, name='manage_metrics'),
     path('metric/create/', views.create_metric, name='create_metric'),
