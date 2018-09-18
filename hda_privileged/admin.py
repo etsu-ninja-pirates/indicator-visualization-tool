083cb0fbd1af4720c6b24bd0ef303eb7d0a0ca93
Matthew Seiler <theflyinghyrax@gmail.com>
2018-09-18 10:13:12 -0400

    Add new models to the admin interface

--- hda_privileged/admin.py
+++ hda_privileged/admin.py
@@ -1,21 +1,42 @@
 from django.contrib import admin
-from .models import US_State, US_County #, Data_Point, Data_Set, Indicators
+from .models import *
 
 # Register your models here.
+
+class Data_Point_Inline(admin.TabularInline):
+    model = Data_Point
+
+class Data_Set_Inline(admin.StackedInline):
+    model = Data_Set
+
+class US_County_Inline(admin.TabularInline):
+    model = US_County
+
 @admin.register(US_State)
-class USState(admin.ModelAdmin):
-    fields = ['short', 'full', 'fips']
+class US_State_Admin(admin.ModelAdmin):
+    inlines = (US_County_Inline,)
+
+    def __str__(self):
+        return "US State"
 
 @admin.register(US_County)
-class USCounties(admin.ModelAdmin):
-    fields = ['fips', 'name', 'state']
+class US_Counties_Admin(admin.ModelAdmin):
+    inlines = (Data_Point_Inline,)
 
-# @admin.register(Data_Point)
-# class DataPoint(admin.ModelAdmin):
-#     fields = []
-# @admin.register(Data_Set)
-# class DataSet(admin.ModelAdmin):
-#     fields = []
-# @admin.register(Indicators)
-# class HIndicators(admin.ModelAdmin):
-#     fields = []
+@admin.register(Health_Indicator)
+class Health_Indicator_Admin(admin.ModelAdmin):
+    inlines = (Data_Set_Inline,)
+
+@admin.register(Document)
+class Document_Admin(admin.ModelAdmin):
+    date_hierarchy = 'uploaded_at'
+    readonly_fields = ('uploaded_at',)
+
+@admin.register(Data_Set)
+class Data_Set_Admin(admin.ModelAdmin):
+    search_fields = ('indicator__name',)
+    inlines = (Data_Point_Inline,)
+
+@admin.register(Data_Point)
+class Data_Point_Admin(admin.ModelAdmin):
+    search_fields = ('county__name', 'county__state__name',)
