79ce6aa5e2f836a07430a1d3cad52a59c56c942e
Johnny E <nshimiyejm@gmail.com>
2018-09-28 15:50:14 -0400

    Des-17-issue displaying grapgh
    There are issues displaying content with 
    1. I used the table to display all the items in the DB
    2. I used chart.html to try printing object using charts.js - https://www.youtube.com/watch?v=B4Vmm3yZPgc  
    
    3. I used chart2.html to try printing/ displaying items using highcharts
    Resources used: 
    1. https://simpleisbetterthancomplex.com/tutorial/2018/04/03/how-to-integrate-highcharts-js-with-django.html 
    
    2. https://stackoverflow.com/questions/27810087/passing-django-database-queryset-to-highcharts-via-json
    
    3. http://jsfiddle.net/umckr21p/

--- hda_public/urls.py
+++ hda_public/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
+from hda_public.views import DashboardView, ChartView, TableView
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
+    path('', DashboardView.as_view(), name='dashboard'),
+    path('chart/', ChartView.as_view(), name='chart-data'),
+    path('table/', TableView.as_view(), name='table'),
 ]
--- hda_public/views.py
+++ hda_public/views.py
@@ -1,4 +1,69 @@
+from django.views.generic import TemplateView
 from django.shortcuts import render
+from django.http import HttpResponse
+from hda_privileged.models import Data_Point
+# from django.db.models import Count, Q
+from django.http import JsonResponse
+from django.core import serializers
+import json
 
-def dashboard(request):
-    return render(request,'hda_public/dashboard.html', context= None)
+
+class ChartView(TemplateView):
+    """ 
+        This class will be used to display Json content that will be return
+        from the char_data function 
+    """
+    template_name = 'hda_public/chart.html'
+
+
+    def get_chart(self, request): 
+        datasets = Data_Point.objects.all() 
+
+        list_counties_data = list()
+        list_values_data = list() 
+        list_percentiles_data = list()
+
+        for entry in datasets: 
+            list_counties_data.append(entry['county_id'])
+            # list_values_data.append(entry['vale'])
+            list_percentiles_data.append(entry['percentile'])
+
+        # value_series = {
+        #     'name'  :  'Values',
+        #     'data'  :  list_values_data, 
+        #     'color' : 'green' 
+        # }
+        
+
+        percentile_series = {
+            'name' : 'Percentile', 
+            'data' : list_percentiles_data 
+        }
+
+        chart = {
+        'chart': {'type': 'column'},
+        'title': {'text': 'Health Indicator'},
+        'xAxis': {'Counties': list_counties_data},
+        'series': [percentile_series]
+         }
+
+        dump = json.dumps(chart)
+
+        # messages.error(request, 'An unexpected error occured.')
+        return render(request, self.template_name,{'chart': dump})
+  
+
+class DashboardView(TemplateView):
+    template_name = 'hda_public/dashboard.html'
+
+    def get_view(self, request): 
+        return render(request,self.template_name)
+
+
+class TableView(TemplateView):
+    template_name = 'hda_public/table.html'
+
+    def get(self, request): 
+        datasets = Data_Point.objects.all()
+        args = {'datasets': datasets}
+        return render(request,self.template_name, args)
