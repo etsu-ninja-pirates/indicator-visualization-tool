ab71a315b52128d8e7a982f8f6707335cefcb0af
Matthew Seiler <theflyinghyrax@gmail.com>
2018-10-02 20:15:16 -0400

    Display data from model in scatter plot

--- hda_public/views.py
+++ hda_public/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render
 from django.http import HttpResponse
-from hda_privileged.models import Data_Point
+from hda_privileged.models import Data_Point, Data_Set
 # from django.db.models import Count, Q
 from django.http import JsonResponse
 from django.core import serializers
@@ -9,61 +9,43 @@ import json
 
 
 class ChartView(TemplateView):
-    """ 
+    """
         This class will be used to display Json content that will be return
-        from the char_data function 
+        from the char_data function
     """
     template_name = 'hda_public/sample2.html'
 
 
-    def get_chart(self, request): 
-        chartdata = json.dumps([[pt.percentile, pt.value] for pt in Data_Point.objects.all()])
-        return render(request, self.template_name, {'chartdata': chartdata})
-        list_counties_data = list()
-        list_values_data = list() 
-        list_percentiles_data = list()
+    def get(self, request):
+        # this ne doesn't plot even though the data is delivered - is it because th object keys end up quoted?
+        def trans(pt):
+            return {
+                'x': pt.percentile * 100,
+                'y': pt.value * 100,
+                'name': pt.county.name,
+            }
 
-        for entry in datasets: 
-            list_counties_data.append(entry['county_id'])
-            # list_values_data.append(entry['vale'])
-            list_percentiles_data.append(entry['percentile'])
+        # for demo purposes, grab the first data set
+        ds = Data_Set.objects.first()
+        # filter for the data points from counties in New York state
+        points = ds.data_points.filter(county__state__short='NY')
+        # transform the points into a list of objects for Highcharts
+        chartdata = json.dumps([trans(pt) for pt in points])
 
-        # value_series = {
-        #     'name'  :  'Values',
-        #     'data'  :  list_values_data, 
-        #     'color' : 'green' 
-        # }
-        
+        return render(request, self.template_name, { 'chartdata': chartdata })
 
-        percentile_series = {
-            'name' : 'Percentile', 
-            'data' : list_percentiles_data 
-        }
-
-        chart = {
-        'chart': {'type': 'column'},
-        'title': {'text': 'Health Indicator'},
-        'xAxis': {'Counties': list_counties_data},
-        'series': [percentile_series]
-         }
-
-        dump = json.dumps(chart)
-
-        # messages.error(request, 'An unexpected error occured.')
-        return render(request, self.template_name,{'chart': dump})
-  
 
 class DashboardView(TemplateView):
     template_name = 'hda_public/dashboard.html'
 
-    def get_view(self, request): 
+    def get_view(self, request):
         return render(request,self.template_name)
 
 
 class TableView(TemplateView):
     template_name = 'hda_public/table.html'
 
-    def get(self, request): 
+    def get(self, request):
         datasets = Data_Point.objects.all()
         args = {'datasets': datasets}
         return render(request,self.template_name, args)
