f18b3393a186cdcdff8624e9367af1918637a019
Johnny E <nshimiyejm@gmail.com>
2018-10-10 18:25:20 -0400

    DES-72 Pulling all years
    Update to the last commit with a method instead of  a class to return all the years of a particular kpi

--- hda_public/tests/test_years.py
+++ hda_public/tests/test_years.py
@@ -1,11 +1,17 @@
 from django.test import TestCase
 from hda_privileged.models import Data_Point, Data_Set, Health_Indicator
+from hda_public.views import showallYears
 
 class SampleGetYearsTest(TestCase):
     def setUp(self): 
-        hi_name = "Obisity"
-        ds = Data_Set.objects.all().filter(indicator__name = hi_name)
-        years = [i.year for i in ds]
-        self.assertEquals(years.count() == 4)
+        self.hi = Health_Indicator(name = 'Obisity')
+        self.hi.save()
+        
+    
+    def test_years_returned(self):
+        Data_Set.objects.create(indicator=self.hi, year = 2016)
+        years = showallYears('Obisity')
+        self.assertEqual(years, [2016])
+
 
         # self.assertTrue(ds.exists())
--- hda_public/views.py
+++ hda_public/views.py
@@ -20,15 +20,21 @@ class ChartView(TemplateView):
                 'y': pt.value * 100,
                 'name': pt.county.name,
             }
-
+        
+        kpi_name = 'Obisity'
+        
+        # function call that returns all the years based on the KPI that is passed to it  
+        years = showallYears(kpi_name)
+        
         # for demo purposes, grab the first data set
-        # ds = Data_Set.objects.first()
+        ds = Data_Set.objects.first()
         # filter for the data points from counties in New York state
-        # points = ds.data_points.filter(county__state__short='TN')
+        points = ds.data_points.filter(county__state__short='NY')
+        # pts = ds.data_points.all()
         # transform the points into a list of objects for Highcharts
         chartdata = json.dumps([trans(pt) for pt in points])
 
-        return render(request, self.template_name, { 'chartdata': chartdata })
+        return render(request, self.template_name, { 'chartdata': chartdata, 'years':years })
 
 
 class DashboardView(TemplateView):
@@ -46,13 +52,10 @@ class TableView(TemplateView):
         args = {'datasets': datasets}
         return render(request,self.template_name, args)
 
-# This class will be used to test the return of all the data sets poining to a particular year 
-class SampleView(TemplateView):
-    template_name = 'hda_public/Sample.html'
-
-    def get(self, request):
-        # for demo purposes, grab the first data set
-        hi_name = 'Obisity'
-        ds = Data_Set.objects.all().filter(indicator__name = hi_name)
-        years = [i.year for i in ds]
-        return render(request,self.template_name, { 'years': years})
+# This method will be used to test the return of all the data sets poining to a particular year 
+def showallYears(kpi_name):
+    """ This function takes in a KPI name then returns all the years liked to it """
+    # for demo purposes, grab the first data set
+    ds = Data_Set.objects.all().filter(indicator__name = kpi_name).order_by('year')
+    years = [i.year for i in ds]
+    return years
