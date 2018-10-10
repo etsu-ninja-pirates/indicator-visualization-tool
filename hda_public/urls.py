f37b5cdede35e48afd99415898c2dafce807c834
Matthew Seiler <theflyinghyrax@gmail.com>
2018-10-10 16:53:20 -0400

    Examples/experiments for using URL parameters in class-based views
    Delete these later

--- hda_public/urls.py
+++ hda_public/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from hda_public.views import DashboardView, ChartView, TableView
+from hda_public.views import DashboardView, ChartView, TableView, TestView, TestTemplateView
 from . import views
 
 urlpatterns = [
     path('', DashboardView.as_view(), name='dashboard'),
     path('chart/', ChartView.as_view(), name='chart-data'),
-    path('table/', TableView.as_view(), name='table')
+    path('table/', TableView.as_view(), name='table'),
+    path('test/<int:year>', TestTemplateView.as_view()),
+    path('test/', TestTemplateView.as_view()),
 ]
--- hda_public/views.py
+++ hda_public/views.py
@@ -1,7 +1,38 @@
+from django.views import View
 from django.views.generic import TemplateView
 from django.shortcuts import render
 from hda_privileged.models import Data_Point, Data_Set
 import json
+
+
+# Example of using URL parameter in a class-based view subclassing "django.views.generic.base.View"
+# The "get" method becomes equivalent to a function-based view: it is called with the URL
+# parameter as a keyword argument, so the parameter needs to be declared as part of the
+# function signature for "get".
+# https://docs.djangoproject.com/en/2.0/ref/class-based-views/base/#view
+#
+# (We *can* access self.kwargs here, but "get" is called with the parameter and Python will crash
+# if you call a function with an undeclared keyword parameter.)
+class TestView(View):
+    def get(self, request, year=None):
+        return render(request, 'hda_public/dashboard.html', {'year': year})
+
+# Example of using URL parameter in a class based view subclassing "django.views.generic.base.TemplateView"
+# TemplateView is intended to abstract over the HTTP methods, and allows you to override "get_context_data"
+# to determine the context that gets passed to the template.
+# https://docs.djangoproject.com/en/2.0/ref/class-based-views/base/#templateview
+#
+# Whenever class-based views are called Django populates the properties `self.request`, `self.args`, and
+# `self.kwargs` - we can pull URL parameters out of self.args (positional) and self.kwargs (keyword),
+# and use them to add to the context object sent to the template.
+# https://docs.djangoproject.com/en/2.1/topics/class-based-views/generic-display/#dynamic-filtering
+class TestTemplateView(TemplateView):
+    template_name = 'hda_public/dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['year'] = self.kwargs.get('year', None)
+        return context
 
 
 class ChartView(TemplateView):
