8c729ce4146d8f9b27ab9fb27d5062bac32433b5
Johnny E <nshimiyejm@gmail.com>
2018-10-11 00:16:25 -0400

    DES-72 Test cases added to pull all the years
    Additional test cases added to pull all the years from the db

--- hda_public/tests/test_years.py
+++ hda_public/tests/test_years.py
@@ -4,14 +4,32 @@ from hda_public.views import showallYears
 
 class SampleGetYearsTest(TestCase):
     def setUp(self): 
-        self.hi = Health_Indicator(name = 'Obisity')
-        self.hi.save()
-        
-    
-    def test_years_returned(self):
-        Data_Set.objects.create(indicator=self.hi, year = 2016)
+        self.h1 = Health_Indicator(name = 'Obisity')
+        self.h1.save()
+
+    #Determine if the number of items in the databes for obisity is one 
+    def test_that_number_of_year_count_for_obisity_is_one_returned(self):
+        Data_Set.objects.create(indicator=self.h1, year = 2014)
         years = showallYears('Obisity')
-        self.assertEqual(years, [2016])
+        self.assertEqual(len(years), 1)
+    
+    def test_if_the_current_year_in_the_database_for_obisity_is_2014(self):
+        Data_Set.objects.create(indicator=self.h1, year = 2014)
+        years = showallYears('Obisity')
+        self.assertEqual(years, [2014])
+    
+    def test_that_one_kpi_can_have_multiple_years(self):
+        Data_Set.objects.create(indicator=self.h1, year = 2014)
+        Data_Set.objects.create(indicator=self.h1, year = 2017)
+        Data_Set.objects.create(indicator=self.h1, year = 2016)
+        Data_Set.objects.create(indicator=self.h1, year = 2018)
+        years = showallYears('Obisity')
+        self.assertEqual(len(years), 4)
 
-
-        # self.assertTrue(ds.exists())
+    def test_that_the_years_are_returned_sorted_in_asc_order(self):
+        Data_Set.objects.create(indicator=self.h1, year = 2014)
+        Data_Set.objects.create(indicator=self.h1, year = 2017)
+        Data_Set.objects.create(indicator=self.h1, year = 2016)
+        Data_Set.objects.create(indicator=self.h1, year = 2018)
+        years = showallYears('Obisity')
+        self.assertEqual(years, [2014, 2016, 2017, 2018])
