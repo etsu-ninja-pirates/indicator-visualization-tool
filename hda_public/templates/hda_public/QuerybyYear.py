74652d3375b682d2d6ace478b84c98fb264e478c
Kim Hawkins <hawkinska1@goldmail.etsu.edu>
2018-10-15 12:56:29 -0400

    Unit Tests
    Test function that takes user year input and returns a queryset for data. All tests when run return "ok" although this is not the desired result

--- hda_public/templates/hda_public/QuerybyYear.py
+++ hda_public/templates/hda_public/QuerybyYear.py
@@ -1,7 +1,7 @@
 from hda_privileged.models import *
 
 def dataByYear(userYear, userIndicator=''):
-    ds = Data_Set.objects.filter(year=userYear, indicator=userIndicator)
+    ds = Data_Set.objects.filter(year=userYear, indicator__name=userIndicator)
     return ds
 
 
