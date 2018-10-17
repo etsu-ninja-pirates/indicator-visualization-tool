ba0d59fadaf85ddce8e91a1854e22e7de54e197d
Kim Hawkins <hawkinska1@goldmail.etsu.edu>
2018-10-17 16:13:30 -0400

    Unit tests for query
    WIP

--- hda_privileged/tests/test_query_des75.py
+++ hda_privileged/tests/test_query_des75.py
@@ -3,7 +3,7 @@ from django.core.management import call_command
 from hda_public.templates.hda_public.QuerybyYear import *
 from hda_privileged.models import Health_Indicator, Data_Set
 
-class UserVarSelect(TestCase):
+class SelectDataForYearTestCase(TestCase):
 
     # setUp runs once before each of the other test cases,
     # so we can use it to set up things that all of our test
@@ -16,6 +16,12 @@ class UserVarSelect(TestCase):
         self.indicator_name = 'A Test Indicator'
         self.health_indicator = Health_Indicator(name=self.indicator_name)
         self.health_indicator.save()
+
+    # Verify no datasets returned when none has been created
+    def test_none_returned(self):
+        name="Returned from empty sandbox"
+        result = dataByYear(2018, userIndicator=self.indicator_name)
+        self.assertFalse(result.exists())
 
     # 1. Set up the inputs/environment for the test
     # (in our case, this means creating things for the database)
@@ -34,21 +40,9 @@ class UserVarSelect(TestCase):
     # This is a good test idea: make sure the year of the result matches the one you ask for
     def test_correct_year_returned(self):
         name = "Correct year returned"
-        correctYear = dataByYear(2018)
+        # set up data to test against
+        Data_Set.objects.create(indicator=self.health_indicator, year=2018)
+        Data_Set.objects.create(indicator=self.health_indicator, year=2017)
+        correctYear = dataByYear(2018, userIndicator=self.indicator_name)
         # since the result is a QuerySet, we need to use .first() to extract the first object in the list of results
-        self.assertEqual(correctYear.first().year, 2017)
-
-    # Also a reasonable idea: but AssertEqual/NotEqual might not be what you want here.
-    # Comparing the two QuerySets for equality might not do what we expect!
-    # Since QuerySet is a class, what happens here depends on whether QuerySet defines an __eq__ method
-    # (just like equals() in Java) and if so, what that implementation is.
-    # So it may be easier to compare the attributes or contents of the QuerySets.
-    def test_assertYearEqual(self):
-        first = dataByYear(2018)
-        second = dataByYear(2017)
-        self.assertEqual(first, second)
-
-
-
-
-
+        self.assertEqual(correctYear.first().year, 2018)
