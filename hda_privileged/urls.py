258c5f5b0a3d4b20a71df88b6241aeabbbfc56bc
Kim Hawkins <hawkinska1@goldmail.etsu.edu>
2018-11-17 12:29:58 -0500

    Cleaned up order, empty spaces

--- hda_privileged/urls.py
+++ hda_privileged/urls.py
@@ -4,12 +4,12 @@ from hda_privileged.views import PrivDashboardView, logout_view
 from . import views
 
 app_name = 'priv'
-urlpatterns = [
-    path('login/', views.user_login, name='login'), 
-    path('home/', login_required(views.PrivDashboardView.as_view(), login_url='priv:login'), name='privdashboard'), 
-    path('home/<slug>/', views.PrivDashboardView.as_view(), name='dashboardselected'),   
+urlpatterns = [     
+    path('home/', login_required(views.PrivDashboardView.as_view(), login_url='priv:login'), name='privdashboard'),
+    path('login/', views.user_login, name='login'),
     path('metric/', views.manage_metrics, name='manage_metrics'),
     path('metric/create/', views.create_metric, name='create_metric'),
     path('upload/', login_required(views.UploadNewDataView.as_view(), login_url='priv:login'), name='upload_metric'),
-    path('logout/', views.logout_view, name='logout'),    
+    path('logout/', views.logout_view, name='logout'),
+    path('home/<slug>/', views.PrivDashboardView.as_view(), name='dashboardselected'),
 ]
