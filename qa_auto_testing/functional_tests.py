81a6a6596a48cae0838acda95717da27d7b40d8d
Johnny E <nshimiyejm@gmail.com>
2019-01-25 13:23:31 -0500

    Setup comments
    DES-162 - Comments describing how to setup local environment to enable automated testing

--- qa_auto_testing/functional_tests.py
+++ qa_auto_testing/functional_tests.py
@@ -1,3 +1,13 @@
+# Steps to follow to get Selenium working in your project: 
+# run `pip install selenium`
+# Add your we driver to the local directory 
+# `geckodriver.exe` or `chromedriver.exe` 
+# e.g.: In our case, the driver is in `qa_auto_testing`
+# folder as the `functional_test.py` script that uses 
+# the driver. Once that is complete, you can start writing 
+# the automated test. 
+
+
 from selenium import webdriver
 
 browser = webdriver.Chrome()
