4e74c8d8adb707c824bdc9d5760467fae1772df7
Matthew Seiler <theflyinghyrax@gmail.com>
2019-02-08 17:23:24 -0500

    Add URLs that return Highcharts data series objects as JSON

--- app_api/urls.py
+++ app_api/urls.py
@@ -4,6 +4,7 @@ import app_api.views.county as county
 import app_api.views.state as state
 
 from app_api.views.search import StateSuggestions, CountySuggestions
+from app_api.views.chart import PercentileSeries, PointSeries
 
 
 app_name = 'api'
@@ -14,4 +15,7 @@ urlpatterns = [
     # search suggestions
     path('search/suggestions/state/<str:query>', StateSuggestions.as_view(), name='suggest_state'),
     path('search/suggestions/county/<str:query>', CountySuggestions.as_view(), name='suggest_county'),
+    # async chart series
+    path('chart/percentiles/<int:data_set_id>/', PercentileSeries.as_view(), name='chart_percentiles'),
+    path('chart/points/<int:data_set_id>', PointSeries.as_view(), name='chart_points'),
 ]
